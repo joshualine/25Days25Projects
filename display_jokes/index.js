7b08d90b9608a0695f62aea0131952c9cc057532
Joshaline <joshuachinwendu@gmail.com>
2021-08-03 23:22:57 +0100

    added drink-water effect

---
+++ display_jokes/index.js
@@ -0,0 +1,10 @@
+const posts = [
+  { title: "Post one", body: "This is post one", category: "science"},
+  { title: "Post two", body: "This is post two", category: "art"},
+  { title: "Post three", body: "This is post Three", category: "science"},
+  { title: "Post four", body: "This is post four", category: "science"},
+  { title: "Post five", body: "This is post five", category: "engineering"},
+  { title: "Post six", body: "This is post six", category: "art"},
+  { title: "Post seven", body: "This is post seven", category: "science"},
+  { title: "Post eight", body: "This is post eight", category: "humanities"},
+];
